605724a006b2b94a949e7c583c76eabec04a5290
Sven Schilke <sven.schilke@ch.abb.com>
2021-04-08 18:01:09 +0200

    extended to clsutering and classification

---
+++ app/app.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MinMaxScaler
+
+
+class Predictor:
+    # get data
+    def get_data(self):
+        file = st.sidebar.file_uploader("Choose a csv file", type="csv")
+        if file is not None:
+            self.data = pd.read_csv(file)
+            return self.data
+        else:
+            st.text("Please upload a CSV File")
+
+    # choose classifier
+    def select_algo(self):
+        self.type = st.sidebar.selectbox("Algorithm", ("Classification", "Clustering"))
+        if self.type == "Classification":
+            self.chosen_classifier = st.sidebar.selectbox(
+                "Classification", ("Random Forest", "Logistic Regression")
+            )
+            if self.chosen_classifier == "Random Forest":
+                self.n_trees = st.sidebar.slider("number of trees", 1, 1000, 1)
+            elif self.chosen_classifier == "Logistic Regression":
+                self.max_iter = st.sidebar.slider(
+                    "Choose maximum number of Iterations", 1, 50, 10
+                )
+        if self.type == "Clustering":
+            pass
+
+    # select features and targets
+    def set_features(self):
+        self.features = st.sidebar.multiselect(
+            "Please choose your features", self.data.columns
+        )
+        if self.type == "Classification":
+            self.target = st.sidebar.selectbox(
+                "Please choose your target variable",
+                [
+                    variable
+                    for variable in self.data.columns
+                    if variable not in self.features
+                ],
+            )
+
+    # preprocess data
+    def preprocess_data(self, test_size):
+        X = self.data[self.features]
+        X = MinMaxScaler().fit_transform(X)
+        if self.type == "Classification":
+            y = self.data[self.target]
+            self.train_X, self.test_X, self.train_y, self.test_y = train_test_split(
+                X, y, test_size=test_size
+            )
+        elif self.type == "Clustering":
+            self.X = X
+
+    # make prediction
+
+
+if __name__ == "__main__":
+    controller = Predictor()
+    controller.data = controller.get_data()
+    if controller.data is not None:
+        controller.select_algo()
+        controller.set_features()
+    if controller.data is not None and len(controller.features) > 0:
+        test_size = st.sidebar.slider("Choose size (%) of test set", 1, 99, 33)
+        controller.preprocess_data(test_size)
